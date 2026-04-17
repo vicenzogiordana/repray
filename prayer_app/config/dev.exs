a33700020c09db5ec7a0ab04d15019d9ad26b2fa
Vicenzo Giordana <giordanavicenzo@gmail.com>
2026-04-17 00:48:21 -0300

    fix: correct regex syntax by removing extraneous 'E' in dev.exs

--- prayer_app/config/dev.exs
+++ prayer_app/config/dev.exs
@@ -58,12 +58,12 @@ config :prayer_app, PrayerAppWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
+      ~r"priv/gettext/.*\.po$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/prayer_app_web/router\.ex$"E,
-      ~r"lib/prayer_app_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/prayer_app_web/router\.ex$",
+      ~r"lib/prayer_app_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
